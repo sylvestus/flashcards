885dc0ea9d31397c7f08c0cf12500f29bbc31474
sylvestus <sylvestus.sigei@student.moringaschool.com>
2022-04-20 22:48:15 +0300

    add profile update templates

--- flashcardsapp/views.py
+++ flashcardsapp/views.py
@@ -1,6 +1,8 @@
 from email import message
 from django.shortcuts import render
 from .models import Notes,Profile,Subject,Additionals
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def home(request):
@@ -8,6 +10,29 @@ def home(request):
     subjects=Subject.objects.all()
 
     return render(request,'home.html',{'message':message,'subjects':subjects})
+
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    message='profile'
+    # projects = Profile.objects.filter(user=current_user.id).all"projects": projects
+    return render(request, 'profile.html', {'message':message })
+
+@login_required(login_url='/accounts/login/')
+def uprofile(request, id):
+    current_user = request.user
+    user_object = get_object_or_404(User, id=id)
+    profile_object = get_object_or_404(Profile, user_id=id)
+    profile_update = UprofileForm(request.POST or None ,request.FILES, instance=profile_object)
+    user_update = UuserForm(request.POST or None, instance=user_object,)
+    if profile_update.is_valid() and user_update.is_valid():
+        user_update.save()
+        profile_update.save()
+        return HttpResponseRedirect("/showProfile")
+
+    return render(
+        request, "uprofile.html",{"profile_update": profile_update, "user_update": user_update})
 
 def flashcards_in_subject(request,id):
     message='flashcards'
