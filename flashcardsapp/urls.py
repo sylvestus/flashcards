f7ec39c7a5573e302efb94cd8a89c8efa936c135
sylvestus <sylvestus.sigei@student.moringaschool.com>
2022-04-20 23:10:41 +0300

    add profile update and show

--- flashcardsapp/urls.py
+++ flashcardsapp/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('newflash/',views.new_flash_card,name='newflash'),
     path('flashcards/<int:id>',views.flashcards_in_subject,name='flashcards'),
-     path('profile/',views.profile,name='profile'),
+    path('profile/',views.profile,name='profile'),
     path('showProfile',views.profile,name='showProfile'),
     path('showProfile/update/<int:id>',views.uprofile,name='uprofile'),
     
