1eaf9ea05a8b59bd787cdd3ca116f3d114f3486a
sylvestus <sylvestus.sigei@student.moringaschool.com>
2022-04-20 22:11:56 +0300

    add database configs

--- flashcardsapp/admin.py
+++ flashcardsapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Profile,Notes,Additionals,Subject
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Subject)
+admin.site.register(Notes)
+admin.site.register(Additionals)
